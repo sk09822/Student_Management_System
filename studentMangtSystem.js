1178e36b354667ba0bd4cdf6f39166d999f3e321
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-02-06 17:38:56 +0530

    Added : add and display the data

--- studentMangtSystem.js
+++ studentMangtSystem.js
@@ -1,1 +1,17 @@
-console.log("welcome to student mangt system";)
+class student{
+  constructor(name,rollNo,address,physicsMarks,chemistryMarks,mathMarks){
+    this.name = name;
+    this.rollNo = rollNo;
+    this.address = address;
+    this.physicsMarks = physicsMarks;
+    this.chemistryMarks = chemistryMarks;
+    this.mathMarks= mathMarks;
+
+  }
+ 
+}
+student1 = new student("Aditya",23,"pune",45,64,54);
+student2 = new student("Shubham",54,"pune",65,76,67);
+console.log(student1);
+console.log(student2);
+  
