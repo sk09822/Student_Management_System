c4f084183b3bbfe6fd642d595d835e0f9fe6a91e
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-02-06 17:35:30 +0530

    Student mangt system

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let student ={
+//   name: "sandipan",
+//   rollNo: 24,
+//   address : "Maharashtra",
+//   physicsMarks :65,
+//   chemistryMarks :30,
+//   mathMarks : 98
+// }
+// console.log(student);
